f2911ebb5ade6a4dbe85b6967c4a881dfa13cce1
Allen Kovacevic <akovacevic4@etf.unsa.ba>
2022-03-25 01:50:33 +0100

    Added user service and validation

--- user-service/user/src/test/java/com/etf/unsa/ba/nwt/user/controller/UserControllerTest.java
+++ user-service/user/src/test/java/com/etf/unsa/ba/nwt/user/controller/UserControllerTest.java
@@ -1,4 +1,49 @@
 package com.etf.unsa.ba.nwt.user.controller;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUsers() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/user/all")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void addUser() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/persons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"name\": \"newnew\",\n" +
+                        "  \"email\": \"test@test.com\"\n" +
+                        "}");
+
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
 }
